db764bd17b6aa4f11e2d663d2b74b7d1e0ad8f91
anonymous <anonymous@users.noreply.github.com>
2023-11-19 15:03:46 +0000

    STAR-52 code review part II

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config';
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
-import { usersRouter } from './routes/usersRoutes';
-import { authRouter } from './routes/authRoutes';
+import { usersRouter } from "./routes/usersRoutes";
+import { authRouter } from "./routes/authRoutes";
 
 export const app = express();
 
@@ -11,12 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 // for a basic check on /api
-app.get('/api', (req: Request, res: Response) => {
-  res.json('STAR Server /api API Route');
+app.get("/api", (req: Request, res: Response) => {
+  res.json("STAR Server /api API Route");
 });
 
-app.use('/api/users', usersRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 export const server = app.listen(process.env.SERVER_PORT, () => {
   console.log(
