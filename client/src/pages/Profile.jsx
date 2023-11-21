1de29c00a764f4fa7457ea733390aaf1ae5ba9b1
Farzaneh Haghani <farzaneh.haghani@students.codeyourfuture.io>
2023-11-21 23:44:33 +0000

    getting all questions in profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,41 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Box, CardMedia, Typography } from "@mui/material";
 import { AuthContext } from "../context/AuthContext";
 
 const Profile = () => {
   // get the userCookie from AuthContext
   const { userCookie } = useContext(AuthContext);
+
+  // define state to store the Users Data
+  const [userQuestionsData, setUserQuestionsData] = useState(null);
+
+  useEffect(() => {
+    // fetch the Users Data from the backend
+    const fetchUserQuestions = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_SERVER_URL}/api/questions/user/${
+            userCookie.id
+          }`,
+          { credentials: "include" } // include HTTP-Only Cookie with customJWT
+        );
+        // console.log("fetchUserQuestions response:", response);
+
+        if (!response.ok) {
+          throw response;
+        }
+
+        const data = await response.json();
+        // console.log("fetchUserQuestions data:", data);
+
+        // store the Users Questions in state
+        setUserQuestionsData(data);
+      } catch (error) {
+        console.error("fetchUserQuestions error:", error);
+      }
+    };
+    fetchUserQuestions();
+  }, []);
 
   const profileBackgroundImage = "/images/background-001.jpg";
 
@@ -38,6 +69,20 @@ const Profile = () => {
           <Typography>{userCookie.email}</Typography>
         </Box>
       </Box>
+      {userQuestionsData && (
+        <Box color={"white"} mt={2}>
+          <Typography variant={"h4"} mb={2}>
+            Your Questions
+          </Typography>
+          {userQuestionsData.map((question) => (
+            <Typography key={question.id}>
+              {question.id} {question.question}{" "}
+              {new Date(question.createdAt).toLocaleString()}{" "}
+              {new Date(question.updatedAt).toLocaleString()}
+            </Typography>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 };
