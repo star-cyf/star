9378165443a379f7d7ae80c64587d3501c708903
Baz Murphy <bazmurphy@gmail.com>
2023-11-24 08:32:04 +0000

    material ui css baseline, roboto font, app grid wrapper, refactor home and hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,36 +1,24 @@
 import { Box, Typography } from "@mui/material";
 
 const Hero = () => {
-  const heroBackgroundImage = "/images/background-001.jpg";
-
   return (
     <Box
-      overflow={"hidden"}
-      border={1}
-      sx={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${heroBackgroundImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-        backgroundRepeat: "no-repeat",
-      }}>
-      <Box
-        display={"flex"}
-        flexDirection={"column"}
-        alignItems={"center"}
-        py={6}
-        color={"white"}>
-        <Typography align={"center"} variant={"h2"} marked={"center"}>
-          STAR - Situation, Task, Action and Result
-        </Typography>
-        <Typography variant={"h5"} align={"center"} mt={6}>
-          STAR solves a business problem.
-        </Typography>
-        <Typography mt={4}>
-          At CodeYourFuture, trainees keep a brag diary to build up a bank of
-          examples of their skills, knowledge and capabilities.
-        </Typography>
-        <Typography mt={4}>Build your future</Typography>
-      </Box>
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      py={6}
+      color={"white"}>
+      <Typography align={"center"} variant={"h2"} marked={"center"}>
+        STAR - Situation, Task, Action and Result
+      </Typography>
+      <Typography variant={"h5"} align={"center"} mt={6}>
+        STAR solves a business problem.
+      </Typography>
+      <Typography mt={4}>
+        At CodeYourFuture, trainees keep a brag diary to build up a bank of
+        examples of their skills, knowledge and capabilities.
+      </Typography>
+      <Typography mt={4}>Build your future</Typography>
     </Box>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,16 @@ import Hero from "../components/Hero";
 import Map from "../components/Map";
 
 const Home = () => {
+  const homeBackgroundImage = "/images/background-001.jpg";
+
   return (
-    <Box border={1}>
+    <Box
+      sx={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${homeBackgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+      }}>
       <Hero />
       <Map />
     </Box>
