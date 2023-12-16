411acf96d86cbc8e1370d1f8525c034ab6b20602
Baz Murphy <bazmurphy@gmail.com>
2023-12-16 08:53:45 +0000

    remove util

--- client/src/utils/isTokenExpired.ts
+++
@@ -1,9 +0,0 @@
-import { AuthenticatedUser } from "../types/auth";
-
-const isTokenExpired = (authenticatedUserLocalStorage: AuthenticatedUser) => {
-  const now = Date.now();
-  const expiresAt = authenticatedUserLocalStorage.expirationTime * 1000;
-  return now >= expiresAt;
-};
-
-export default isTokenExpired;
