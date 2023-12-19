6f714f5605f6484164248cb201c304093b19e99a
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-12-19 19:10:27 +0000

    adjustments

--- client/src/components/ServerStatus.tsx
+++ client/src/components/ServerStatus.tsx
@@ -1,35 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Box, Typography } from "@mui/material";
 
 const ServerStatus = () => {
+  console.log("Server Status rendered");
+
   const [isServerAlive, setIsServerAlive] = useState(false);
 
-  const getServerStatus = async () => {
+  const getServerStatus = useCallback(async () => {
     try {
+      console.log("getServerStatus ran");
       const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api`);
-      if (response.status === 200) {
+      if (response.status === 200 && !isServerAlive) {
         setIsServerAlive(true);
       }
     } catch (error) {
       console.error(error);
     }
-  };
+  }, [isServerAlive]);
 
   useEffect(() => {
+    console.log("Server Status useEffect ran");
     getServerStatus();
 
     let intervalId: number;
 
     if (!isServerAlive) {
+      console.log("Server Status useEffect IF BLOCK");
       intervalId = setInterval(() => {
         getServerStatus();
       }, 1500);
     }
 
     return () => {
-      clearTimeout(intervalId);
+      clearInterval(intervalId);
     };
-  }, []);
+  }, [getServerStatus, isServerAlive]);
 
   return (
     <Box display={"flex"} alignItems={"center"} flexWrap={"wrap"} gap={0.5}>
@@ -38,7 +43,7 @@ const ServerStatus = () => {
         component={"span"}
         color={isServerAlive ? "green" : "red"}
         fontSize={12}>
-        {isServerAlive ? "Alive" : "Asleep"}
+        {isServerAlive ? "Alive" : "Waking Up"}
       </Typography>
     </Box>
   );
